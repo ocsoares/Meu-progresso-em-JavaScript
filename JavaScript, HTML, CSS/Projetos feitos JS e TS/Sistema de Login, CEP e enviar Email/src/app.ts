e112d6203bbf89e8e326ac1b7883452b4199affc
ocsoares <casoares016@gmail.com>
2022-07-31 00:22:20 -0300

    dia 31 - concertando alguns bugs do babel e html

--- JavaScript, HTML, CSS/Projetos feitos JS e TS/Sistema de Login, CEP e enviar Email/src/app.ts
+++ JavaScript, HTML, CSS/Projetos feitos JS e TS/Sistema de Login, CEP e enviar Email/src/app.ts
@@ -9,8 +9,16 @@ import checkStatusRoute from './routes/check-status.route';
 import crudRoute from './routes/crud.route';
 
 // IMPORTANTE: Para Compilar de .ts para .js Usando Babel usei o comando:
-// 1 - npm build (DEPOIS ARRUMAR MAIS),
-// 2 - browserify .\testeeee\script-html.js -o build.js (DEPOIS ARRUMAR MAIS e Colocar Watch igual no nodemon !!!!)
+// 1 - npm build - Está com nodemon para Atualizar a cada Modificação, e o Arquivo FINAL (.js) sairá em /dist/script-html !! <<<
+// 2 - browserify:run - Mudar MANUALMENTE o Nome dos Arquivos (Entrada e Saída) no package.json, porque ele só Transpila UM por Vez (Também po-
+// de juntar VÁRIOS Arquivos em apenas UM, se precisar...) !! <<<
+// 3 - Usar o .js FEITO Pelo browserify:run no HTML em <script> !!! <<<
+
+// Para resolver o erro: "Error: Can't walk dependency graph: Cannot find module '...' from ...", COLOCAR no script do browserify ./ na PAS-
+// -TA >Inicial< !! <<<
+
+// Para resolver o Erro: "Cannot find the name 'document'. Do you need to change your target library? Try changing the compiler's 'lib' opti-
+// -on to include 'dom'.ts(2584)" adicionei "DOM" em "lib" no tsconfig.json e Recarreguei a Janela do VSCode !! <<
 
 
 AppDataSource.initialize().then(() => {
--- JavaScript, HTML, CSS/Projetos feitos JS e TS/Sistema de Login, CEP e enviar Email/src/script-html/script-html.ts
+++ JavaScript, HTML, CSS/Projetos feitos JS e TS/Sistema de Login, CEP e enviar Email/src/script-html/script-html.ts
@@ -1,5 +1,29 @@
 import axios from "axios";
 
+// IMPORTANTE: Coloquei o <script> no FINAL do Arquivo .html porque no Topo NÃO estava Lendo !! << 
+//  OBS: OU usar no Topo e colocar defer no final do script, porque ai Espera Carregar o HTML Primeiro << !!
+
+// IMPORTANTE: Coloquei type="button" no Botão de Enviar (html) APENAS para NÃO atualizar a Página Automaticamente, DEPOIS que Arrumar to-
+// -do o Script, voltar para "submit" !! << 
+
+const searchCEP = () => {
+    return console.log('teste...')
+}
+
+const sendInputButton = document.getElementById('send');
+const getCEPID = document.getElementById('cep') as HTMLInputElement; // COLOCAR isso para Permitir o uso de .value e afins !! <<
+
+sendInputButton?.addEventListener('click', clicked => {
+    const getCEPValue = getCEPID.value
+    console.log(getCEPValue);
+})
+
+document.getElementById('cep')?.addEventListener('keypress', anykey => {
+    if(anykey.key === 'Enter'){
+        searchCEP()
+    }
+})
+
 const cep = 11608545;
 
 const url = `https://viacep.com.br/ws/${cep}/json`;
